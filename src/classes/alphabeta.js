d055d5499fcaad2162315dee39f408c2e9d970ab
Roy Dey <roydey@ucdavis.edu>
2020-11-27 21:13:08 -0800

    add helper function comments

--- src/classes/alphabeta.js
+++ src/classes/alphabeta.js
@@ -1,15 +1,18 @@
 import {CELL_STATES, GAME_STATES} from '../utils/enum.js'
 import {get_game_state} from '../utils/helper.js'
-
+/*
+ * Alpha-Beta searches through game states similar to Minimax except
+ * it refuses to search game moves which a competent opponent would
+ * never make, thereby increasing the speed at which it searches. 
+ * This increased speed allows the algorithm to search at greater
+ * depths than Minimax.
+ */
 export default class AlphaBetaAi {
     constructor(player_number, num_to_win, timeout, depth) {
         this.timeout = timeout
         this.chosen_move = 0;
         this.player_num = player_number;
         this.num_to_win = num_to_win;
-        this.num_won = 0;
-        this.num_draw = 0;
-        this.num_lost = 0;
         this.board = null;
         this.depth = depth;
     }
@@ -50,6 +53,7 @@ export default class AlphaBetaAi {
             }
         }
 
+        // If we haven't found a move to make, pick the first column available
         if(!this.can_make_move(best_move)) {
             best_move = 0;
             while(!this.can_make_move(best_move))
@@ -58,7 +62,16 @@ export default class AlphaBetaAi {
         return best_move;
     }
 
-
+    /*
+     * Get the best possible score for your move
+     * @depth: An int representing how many more moves in advance to survey
+     * @player: The current player (1 or 2)
+     * @alpha: The players best score of type int
+     * @beta: The opponents best score of type int
+     * @row: The row where a move has been made
+     * @col: the column where a move has been made
+     * Return: The AI's best possible score
+     */
     max_value(depth, player, alpha, beta, row, col) {
         var best_score = Number.MIN_SAFE_INTEGER;
         var move_score;
@@ -84,6 +97,16 @@ export default class AlphaBetaAi {
         return best_score;
     }
 
+    /*
+     * Get the worst possible score for your opponent
+     * @depth: An int representing how many more moves in advance to survey
+     * @player: The current player (1 or 2)
+     * @alpha: The players best score of type int
+     * @beta: The opponents best score of type int
+     * @row: The row where a move has been made
+     * @col: the column where a move has been made
+     * Return: The opponents worst possible score
+     */
     min_value(depth, player, alpha, beta, row, col) {
         var best_score = Number.MAX_SAFE_INTEGER;
         var move_score;
@@ -154,6 +177,12 @@ export default class AlphaBetaAi {
 
     }
 
+    /*
+     * Determine if a move will result in a win
+     * @move: An int representing the column to make a move in
+     * @curr_player: The current player (1 or 2)
+     * Return: True if the move results in a win, False otherwise
+     */
     is_winning_move(move, curr_player){
         if(this.can_make_move(move)){
             var[r,c] = this.make_move(move, curr_player);
@@ -165,7 +194,11 @@ export default class AlphaBetaAi {
         return false;
     }
     
-
+    /*
+     * get the number of tokens in a column
+     * @col: An int representing the column number
+     * Return: The int number of tokens in a column
+     */
     get_column_height(col) {
         var height = 0;
         for(var i = this.board.length-1; i >= 0; --i){
@@ -177,13 +210,20 @@ export default class AlphaBetaAi {
     }
 
     /* 
-     * checks if the AI has run out of time to make a move
+     * Checks if the AI has run out of time to make a move
+     * @start_time: int time in miliseconds
+     * Return: True if AI has exceeded time limit, False otherwise
      */
     is_timeout(start_time) {
         return (new Date().getTime() - start_time >= this.timeout);
     }
 
-
+    /*
+     * Makes a move on the board at the specified column
+     * @column: An int representing the column to make a move
+     * @curr_player: The player making the move (1 or 2)
+     * Return: int array of the form [row,col] representing where the move was made
+     */
     make_move(column, curr_player) {
         var row = 0;
         while( row < this.board.length && this.board[row][column] === CELL_STATES.EMPTY) ++row;
@@ -192,12 +232,22 @@ export default class AlphaBetaAi {
          
     }
 
-    unmake_move(column) {
+    /*
+     * Unmakes a move at a specified column
+     * @col: An int representing the column
+     * Return: Nothing is returned
+     */
+    unmake_move(col) {
         var row = 0;
-        while( row < this.board.length && this.board[row][column] === CELL_STATES.EMPTY) ++row;
-        this.board[row][column] = CELL_STATES.EMPTY;
+        while( row < this.board.length && this.board[row][col] === CELL_STATES.EMPTY) ++row;
+        this.board[row][col] = CELL_STATES.EMPTY;
     }
     
+    /*
+     * Determines if a move can be made at a specified column
+     * @col: An int representing the column
+     * Return: True if a move can be made in the column, false otherwise
+     */
     can_make_move(column) {
         return this.board[0][column] === CELL_STATES.EMPTY;
     }
